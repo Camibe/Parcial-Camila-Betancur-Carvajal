03f504bfe6d2125f7375e515c335e5d0154c8b83
Camibe <camibecarvajal@gmail.com>
2023-03-07 14:51:43 -0500

    Parcial completo

--- parcial1-front/src/components/Card.jsx
+++ parcial1-front/src/components/Card.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import "./Form.css"
 
-export const Card = () => {
+const Card = ({user}) => {
+
   return (
-    <div>Card</div>
+    <div className='card'>
+       <h4>Hola {user.nombre}, gracias por confiar en nosotros para adoptar tu {user.mascota}, por tu edad actual de: {user.edad} años sabemos que lo disfrutarás muchísimo, pronto llegara a la puerta de tu casa en esta direccion: {user.direccion},
+       al correo {user.email} te llegará el resto de la información</h4>
+       <><h2>Lindo día</h2></>
+    </div>
   )
 }
+export default Card
+
+/*intenté ponerle estilos pero no es mi fuerte, lo demás creo que está bien. Gracias profe :)*/
--- parcial1-front/src/components/Form.jsx
+++ parcial1-front/src/components/Form.jsx
@@ -1,8 +1,53 @@
-import React from 'react'
-import { Card } from './Card'
+import React, { useState } from 'react'
+import './Card.jsx'
+import Card from './Card.jsx'
+import "./Form.css"
 
 export const Form = () => {
+    const [user, setUser] = useState({
+        nombre: '',
+        direccion: '',
+        edad: '',
+        email: '',
+        mascota: ''
+    })
+
+    const [show, setShow] = useState(false)
+    const [err, setErr] = useState(false)
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        if(user.nombre.length > 3 && user.direccion.length > 6) {
+            setShow(true)
+            setErr(false)
+        } else {
+            setErr(true)
+        }
+
+    }
+
   return (
-    <Card/>
+    <div>
+        <h1 className='bienvenido'>Bienvenido a Adopta Conmigo</h1>
+        <form onSubmit={handleSubmit} className= "Form">
+            <label>Nombre Completo</label>
+            <input type="text" value={user.nombre} onChange={(e) => setUser({...user, nombre: e.target.value})}/>
+            <label>Direccion</label>
+            <input type="text" value={user.direccion} onChange={(e) => setUser({...user, direccion: e.target.value})}/>
+            <label>Edad</label>
+            <input type="text" value={user.edad} onChange={(e) => setUser({...user, edad: e.target.value})}/>
+            <label>Email</label>
+            <input type="email" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})}/>
+            <select value={user.mascota} onChange={(e) => setUser({...user, mascota: e.target.value})}>
+                <option value="">Deseo adoptar un</option>
+                <option value="perrito">Perro</option>
+                <option value="gatico">Gato</option>
+                <option value="hamster">Hamster</option>
+            </select>
+            <button>Enviar</button>
+            {err && "Por favor chequea que la información sea correcta"}
+        </form>
+        {show && <Card user={user}/>}
+    </div>
   )
 }
